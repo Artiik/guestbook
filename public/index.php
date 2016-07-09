bd64b2f1eb18843d51b27aeb9c7bbc71a627cd0f
Artiik <artem-makovetskiy@mail.ru>
2016-07-09 05:13:22 +0000

    added local db file

--- public/index.php
+++ public/index.php
@@ -10,9 +10,15 @@ require_once '../vendor/autoload.php';
 //
 //dump($arr);
 
+$file = '../storage/database.db';
+
+if (is_writable('../storage/database.local.db')) {
+    $file = '../storage/database.local.db';
+}
+
 $database = new medoo([
     'database_type' => 'sqlite',
-    'database_file' => '../storage/database.db'
+    'database_file' => $file
 ]);
 
 $comment = new SitePoint\Comment($database);
